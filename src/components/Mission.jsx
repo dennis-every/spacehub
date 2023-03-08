92f3554077c2ed46b62bd2b7be0b58138304426e
Dennis Every <dennis@ansyra.com>
2023-03-08 18:44:29 -0400

    Add joinMission action

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -1,27 +1,51 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { joinMission } from '../redux/missions/missionsSlice';
 
-const Mission = (props) => {
-  const {
-    id, name, description,
-  } = props;
+const Mission = ({ mission }) => {
+  const dispatch = useDispatch();
+
+  const joinMissionHandler = (id) => {
+    dispatch(joinMission(id));
+  };
+
   return (
-    <tr key={id}>
-      <td>{name}</td>
-      <td>{description}</td>
+    <tr>
+      <td>{mission.mission_name}</td>
+      <td>{mission.description}</td>
       <td>
-        <button type="button">NOT A MEMBER</button>
+        {mission.reserved === true ? (
+          <button type="button">Active Member</button>
+        ) : (
+          <button type="button">Not a Member</button>
+        )}
       </td>
       <td>
-        <button type="button">Join Mission</button>
+        <button
+          className="btn btn-outline-secondary btn-sm"
+          type="button"
+          onClick={() => joinMissionHandler(mission.mission_id)}
+        >
+          Join Mission
+        </button>
       </td>
     </tr>
   );
 };
 
 Mission.propTypes = {
-  id: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
+  mission: PropTypes.shape({
+    mission_id: PropTypes.string.isRequired,
+    mission_name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    reserved: PropTypes.bool,
+  }),
+};
+
+Mission.defaultProps = {
+  mission: {
+    reserved: false,
+  },
 };
 
 export default Mission;
