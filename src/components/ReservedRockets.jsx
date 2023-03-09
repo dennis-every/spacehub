d57b939732e88623e105fc673d89259e14adfc68
dfsalam <dfsalam@gmail.com>
2023-03-09 12:59:42 -0500

    Add conditional inside useEffect to save reservations

--- src/components/ReservedRockets.jsx
+++ src/components/ReservedRockets.jsx
@@ -6,7 +6,16 @@ const ReservedRockets = () => {
   const reserved = rockets.filter((rocket) => rocket.reserved === true);
   console.log(reserved);
   return (
-    <div>ReservedRockets</div>
+    <>
+      <h3>My Rockets</h3>
+      <ul className="list-group">
+        {
+          reserved.map((rocket) => (
+            <li key={rocket.rocketId} className="list-group-item">{rocket.name}</li>
+          ))
+        }
+      </ul>
+    </>
   );
 };
 
